bfed619978f17eb2b5fedb809251eebe21bc499b
jheintel <46928339+jheintel@users.noreply.github.com>
2022-02-13 19:28:07 -0500

    finished assignment 1

---
+++ Assignment1A.sql
@@ -0,0 +1,4 @@
+-- query selects everything within the customers table
+-- Jon Heintel 2/13/22
+
+select * from customers
---
+++ Assignment1D.sql
@@ -0,0 +1,6 @@
+-- select all invoices with the vendor_id 123
+-- Jon Heintel 2/13/22
+
+select *
+from invoices
+where vendor_id = 123
