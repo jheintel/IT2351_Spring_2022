7bc0faef897bb3f58c2991c11d7db1da8fdad60b
jheintel <46928339+jheintel@users.noreply.github.com>
2022-02-28 01:08:05 -0500

    uploading task 1

---
+++ Assignment3/JonHeintelTask1sql.sql
@@ -0,0 +1,7 @@
+# The hash makes comments too?
+-- I don't know whom to trust
+
+-- Make a new row in the terms table with the given values
+
+insert into terms(terms_id, terms_description, terms_due_days)
+values (6, "Net due 60 Days", 60)
